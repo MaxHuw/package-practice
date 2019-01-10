be352f70bee65f2581cb1052102fe87bcf595b04
Max Graham <m.huw.graham@gmail.com>
2019-01-10 23:49:43 +0000

    Playing around with chalk and colours

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-var chalk = require("chalk");
+const chalk = require("chalk");
+const log = console.log;
 
-var message = "Hello " + chalk.yellow("World");
-console.log(message);
+
+var message = chalk.blue("Hello ") + chalk.yellow("World");
+log(message);
+
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+const error = chalk.bold.red.dim;
+const warning = chalk.keyword('orange');
+
+log(error('Error!'));
+log(warning('Warning!'));
